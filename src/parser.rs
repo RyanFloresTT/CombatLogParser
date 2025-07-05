b13d23329be27980d058919a2f03d21ea46cb61b
Ryan Flores <rryanflorres@gmail.com>
2025-07-05 11:29:08 -0700

    refactor: covert static fields from parser into instance fields

--- src/parser.rs
+++ src/parser.rs
@@ -1,14 +1,5 @@
 ﻿use crate::models::LogEvent;
 use regex::Regex;
-use lazy_static::lazy_static;
-
-lazy_static! {
-    static ref DUNGEON_RE: Regex = Regex::new(r#"CHALLENGE_MODE_START,"([^"]+)""#).unwrap();
-    static ref COMBATANT_RE: Regex = Regex::new(r#"COMBATANT_INFO,(Player-[^,]+)"#).unwrap();
-    static ref GUID_NAME_RE: Regex = Regex::new(r#"(Player-[^,]+),"([^"]+)""#).unwrap();
-    static ref DAMAGE_RE: Regex = Regex::new(r#"SPELL_DAMAGE,.*,(Player-[^,]+),[^,]*,[^,]*,[^,]*,(\d+),"#).unwrap();
-    static ref DUNGEON_END_RE: Regex = Regex::new(r#"CHALLENGE_MODE_END"#).unwrap();
-}
 
 /* TODO:
     1. Keep track of current dungeon
@@ -17,33 +8,39 @@ lazy_static! {
     4. Stop reading lines when a dungeon has ended, repeat once a new dungeon is found
  */
 pub fn parse_line(line: &str) -> LogEvent {
-    if let Some(cap) = DUNGEON_RE.captures(line) {
+    let dungeon_re = Regex::new(r#"CHALLENGE_MODE_START,"([^"]+)""#).unwrap();
+    let combatant_re = Regex::new(r#"COMBATANT_INFO,(Player-[^,]+)"#).unwrap();
+    let guid_name_re = Regex::new(r#"(Player-[^,]+),"([^"]+)""#).unwrap();
+    let damage_re = Regex::new(r#"SPELL_DAMAGE,.*,(Player-[^,]+),[^,]*,[^,]*,[^,]*,(\d+),"#).unwrap();
+    let dungeon_end_re = Regex::new(r#"CHALLENGE_MODE_END"#).unwrap();
+
+    if let Some(cap) = dungeon_re.captures(line) {
         return LogEvent::DungeonStart {
             name: cap[1].to_string(),
             timestamp: extract_timestamp(line),
         };
     }
 
-    if DUNGEON_END_RE.is_match(line) {
+    if dungeon_end_re.is_match(line) {
         return LogEvent::DungeonEnd {
             timestamp: extract_timestamp(line),
         };
     }
 
-    if let Some(cap) = COMBATANT_RE.captures(line) {
+    if let Some(cap) = combatant_re.captures(line) {
         return LogEvent::CombatantInfo {
             guid: cap[1].to_string(),
         };
     }
 
-    if let Some(cap) = GUID_NAME_RE.captures(line) {
+    if let Some(cap) = guid_name_re.captures(line) {
         return LogEvent::GuidToName {
             guid: cap[1].to_string(),
             name: cap[2].to_string(),
         };
     }
 
-    if let Some(cap) = DAMAGE_RE.captures(line) {
+    if let Some(cap) = damage_re.captures(line) {
         return LogEvent::SpellDamage {
             guid: cap[1].to_string(),
             amount: cap[2].parse().unwrap_or(0),
